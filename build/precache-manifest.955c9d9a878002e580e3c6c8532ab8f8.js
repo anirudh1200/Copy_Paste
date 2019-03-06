50de5ee99a394811515da35dadfc73fdeffd0122
= <anirudh.b.123@gmail.com>
2019-03-06 21:20:30 +0530

    fixes for production

---
+++ build/precache-manifest.955c9d9a878002e580e3c6c8532ab8f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba7f9c91283d6c1d2cc5d6c69aeaa73",
+    "url": "/static/media/background.eba7f9c9.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b43dea57cf8f7e05e35e",
+    "url": "/static/js/main.b43dea57.chunk.js"
+  },
+  {
+    "revision": "3b28224b359369271375",
+    "url": "/static/js/1.3b28224b.chunk.js"
+  },
+  {
+    "revision": "b43dea57cf8f7e05e35e",
+    "url": "/static/css/main.8b363989.chunk.css"
+  },
+  {
+    "revision": "f46779d162344c27d97ffd08d9e453ed",
+    "url": "/index.html"
+  }
+];
